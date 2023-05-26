73066fa5658ab43b6c4352b1c4ce6f47618d484e
riskalaluna <riskalaluna@gmail.com>
2023-05-26 11:35:18 +0700

    22

--- Doublelinkedlist_176/Doublelinkedlist_176.cpp
+++ Doublelinkedlist_176/Doublelinkedlist_176.cpp
@@ -74,3 +74,13 @@ void DoubleLinkedList::addNode() {
 		current->next->prev = newNode;	//step 6
 	current->next = newNode;	//step 7
 }
+
+bool DoubleLinkedList::search(int rollNo, Node** previous, Node** current) {
+	*previous == NULL;	//step 1.a
+	*current = START;	//step 1.b
+	while (*current != NULL && (*current)->noMhs != rollNo) {	//step 1.c
+		*previous = *current;	//step 1.d
+		*current = (*current)->next;	//step 1.e
+	}
+	return (*current != NULL);
+}
