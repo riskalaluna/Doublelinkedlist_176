8437441ff42f7777a28f52a46becdde15a96883c
riskalaluna <riskalaluna@gmail.com>
2023-05-26 11:24:38 +0700

    19

--- Doublelinkedlist_176/Doublelinkedlist_176.cpp
+++ Doublelinkedlist_176/Doublelinkedlist_176.cpp
@@ -62,4 +62,9 @@ void DoubleLinkedList::addNode() {
 		previous = current;	//step 1.d
 		current = current->next;	//step 1.e
 	}
+
+	if (current->next != NULL && nim == current->next->noMhs) {
+		cout << "\nDuplicate roll numbers not allowed" << endl;
+		return;
+	}
 }
