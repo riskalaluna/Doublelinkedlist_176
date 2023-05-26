dad0089f8609d0473f042951da003e0d98be7651
riskalaluna <riskalaluna@gmail.com>
2023-05-26 11:08:32 +0700

    step 2

--- Doublelinkedlist_176/Doublelinkedlist_176.cpp
+++ Doublelinkedlist_176/Doublelinkedlist_176.cpp
@@ -37,4 +37,6 @@ void DoubleLinkedList::addNode() {
 	cout << "\nEnter the name of the student: ";
 	cin >> nm;
 	Node* newNode = new Node();	//step 1
+	newNode->noMhs = nim;	//step 2
+	newNode->name = nm;	//step 2
 }
