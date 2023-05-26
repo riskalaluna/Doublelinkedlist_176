68d7d9fc05f2b549ac6edf6cbcd4cd90bf87fbf1
riskalaluna <riskalaluna@gmail.com>
2023-05-26 15:14:49 +0700

    24

--- Doublelinkedlist_176/Doublelinkedlist_176.cpp
+++ Doublelinkedlist_176/Doublelinkedlist_176.cpp
@@ -98,4 +98,23 @@ bool DoubleLinkedList::deleteNode(int rollNo) {
 		START = current->next;
 
 	delete current; //step 4
+	return true;
 }
+
+bool DoubleLinkedList::listEmpty() {
+	return (START == NULL);
+}
+
+void DoubleLinkedList::traverse() {
+	if (listEmpty())
+		cout << "\nList is empty" << endl;
+	else {
+		cout << "\nRecords in ascending order of roll number are:" << endl;
+		Node* currentNode = START;
+		while (currentNode != NULL) {
+			cout << currentNode->noMhs << " " << currentNode->name << endl;
+			currentNode = currentNode->next;
+		}
+	}
+}
+
