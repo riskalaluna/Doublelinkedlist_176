7939a1da3bebe5bb7cca05f78eadfec4fa433860
riskalaluna <riskalaluna@gmail.com>
2023-05-26 11:07:07 +0700

    step 1

--- Doublelinkedlist_176/Doublelinkedlist_176.cpp
+++ Doublelinkedlist_176/Doublelinkedlist_176.cpp
@@ -34,4 +34,7 @@ void DoubleLinkedList::addNode() {
 	string nm;
 	cout << "\nEnter the roll number of the student: ";
 	cin >> nim;
+	cout << "\nEnter the name of the student: ";
+	cin >> nm;
+	Node* newNode = new Node();	//step 1
 }
